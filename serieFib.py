1c51aaa18cfc4a0adf2fa1118ec749d0e927a313
Levihernandez02 <99519247+Levihernandez02@users.noreply.github.com>
2024-10-14 07:07:33 -0600

    Actualizar serieFib.py

--- serieFib.py
+++ serieFib.py
@@ -15,3 +15,27 @@ resultado = fibonacci(n)
 # Aqui solo se imprimen los resultados con los valores n esimos solicitados para ser visualizados
 for i in range(n):
     print(f"Fibonacci({i}) = {resultado[i]}")
+
+#*************************************
+# Serie de fibonacci con arreglos
+
+def fibonacci(n):
+    # Crear una lista de tamaño n
+    fib = [0] * n
+    
+    # Los primeros dos elementos de la serie de Fibonacci son 0 y 1
+    if n > 0:
+        fib[0] = 0
+    if n > 1:
+        fib[1] = 1
+    
+    # Calcular el resto de la serie
+    for i in range(2, n):
+        fib[i] = fib[i - 1] + fib[i - 2]
+    
+    return fib
+
+# Ejemplo de uso para fibonacci igual a 10 
+n = 10
+serie_fibonacci = fibonacci(n)
+print(serie_fibonacci)
